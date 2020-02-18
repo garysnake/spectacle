8b4163999ce34e7b1bb8811b787dd7d5cdbbc76b
Gary Zhong <garysnake@Garys-MBP.attlocal.net>
2020-02-17 23:31:22 -0600

    Change centering Window Position

--- Spectacle/Resources/Window Position Calculations/SpectacleCenterWindowCalculation.js
+++ Spectacle/Resources/Window Position Calculations/SpectacleCenterWindowCalculation.js
@@ -1,8 +1,13 @@
 var SpectacleCenterWindowCalculation = (function () {
     var centerWindowRectInVisibleFrameOfScreen = function(windowRect, visibleFrameOfScreen) {
         var calculatedWindowRect = SpectacleCalculationHelpers.copyRect(windowRect);
-        calculatedWindowRect.x = Math.round((visibleFrameOfScreen.width - windowRect.width) / 2.0) + visibleFrameOfScreen.x;
-        calculatedWindowRect.y = Math.round((visibleFrameOfScreen.height - windowRect.height) / 2.0) + visibleFrameOfScreen.y;
+//        calculatedWindowRect.x = Math.round((visibleFrameOfScreen.width - windowRect.width) / 2.0) + visibleFrameOfScreen.x;
+//        calculatedWindowRect.y = Math.round((visibleFrameOfScreen.height - windowRect.height) / 2.0) + visibleFrameOfScreen.y;
+        calculatedWindowRect.x = visibleFrameOfScreen.x + Math.floor(visibleFrameOfScreen.width / 3.0);
+        calculatedWindowRect.y = visibleFrameOfScreen.y + Math.floor(visibleFrameOfScreen.height / 3.0);
+        // Add custom Center windows calculation
+        calculatedWindowRect.width = Math.floor(visibleFrameOfScreen.width / 3.0);
+        calculatedWindowRect.height = Math.floor(visibleFrameOfScreen.height / 3.0);
         return calculatedWindowRect;
     };
     return {
